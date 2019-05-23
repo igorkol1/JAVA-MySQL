83c60b6e49b9a29897ea7478d2ff2a5009cf2c00
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-05-23 21:37:05 +0200

    Rozwiązanie zadań z dzień 2 sekcja d cz.1

--- a_Zadania/a_Dzien_2/d_Relacje_1_wiele/Main1.java
+++ a_Zadania/a_Dzien_2/d_Relacje_1_wiele/Main1.java
@@ -2,22 +2,24 @@ package a_Zadania.a_Dzien_2.d_Relacje_1_wiele;
 
 
 public class Main1 {
-    //poniżej zapisz zapytania dodające rekordy do tabeli
-    public String queryProduct1Opinion1 = "";
-    public String queryProduct1Opinion2 = "";
-    public String queryProduct1Opinion3 = "";
-    public String queryProduct1Opinion4 = "";
-    public String queryProduct1Opinion5 = "";
 
-    public String queryProduct2Opinion1 = "";
-    public String queryProduct2Opinion2 = "";
-    public String queryProduct2Opinion3 = "";
-    public String queryProduct2Opinion4 = "";
-    public String queryProduct2Opinion5 = "";
+    //create table opinions(id int AUTO_INCREMENT PRIMARY KEY,description varchar(500),product_id int NOT NULL , FOREIGN KEY(product_id) REFERENCES products(id));
 
-    public String queryProduct3Opinion1 = "";
-    public String queryProduct3Opinion2 = "";
-    public String queryProduct3Opinion3 = "";
-    public String queryProduct3Opinion4 = "";
-    public String queryProduct3Opinion5 = "";
+    public String queryProduct1Opinion1 = "insert into opinions(description, product_id) VALUES ('Dobry bo razowy',1);";
+    public String queryProduct1Opinion2 = "insert into opinions(description, product_id) VALUES ('Nie dobry bo razowy',1);";
+    public String queryProduct1Opinion3 = "insert into opinions(description, product_id) VALUES ('Nie dobry bo suchy',1);";
+    public String queryProduct1Opinion4 = "insert into opinions(description, product_id) VALUES ('Dobry bo suchy',1);";
+    public String queryProduct1Opinion5 = "insert into opinions(description, product_id) VALUES ('Nie dobry bo czerstwy',1);";
+
+    public String queryProduct2Opinion1 = "insert into opinions(description, product_id) VALUES ('Dobra bo tania',2);";
+    public String queryProduct2Opinion2 = "insert into opinions(description, product_id) VALUES ('Moze byc',2);";
+    public String queryProduct2Opinion3 = "insert into opinions(description, product_id) VALUES ('Rozpuszcona',2);";
+    public String queryProduct2Opinion4 = "insert into opinions(description, product_id) VALUES ('Nie dobra bo bez samku',2);";
+    public String queryProduct2Opinion5 = "insert into opinions(description, product_id) VALUES ('Nie dobra bo tlusta',2);";
+
+    public String queryProduct3Opinion1 = "insert into opinions(description, product_id) VALUES ('Test 1',3);";
+    public String queryProduct3Opinion2 = "insert into opinions(description, product_id) VALUES ('Test 2',3);";
+    public String queryProduct3Opinion3 = "insert into opinions(description, product_id) VALUES ('Test 3',3);";
+    public String queryProduct3Opinion4 = "insert into opinions(description, product_id) VALUES ('Test 4',3);";
+    public String queryProduct3Opinion5 = "insert into opinions(description, product_id) VALUES ('Test 5',3);";
 }
--- a_Zadania/a_Dzien_2/d_Relacje_1_wiele/Main2.java
+++ a_Zadania/a_Dzien_2/d_Relacje_1_wiele/Main2.java
@@ -2,10 +2,9 @@ package a_Zadania.a_Dzien_2.d_Relacje_1_wiele;
 
 
 public class Main2 {
-//zapisz poniżej zapytania tworzące odpowiednie tabele
+    
+    public String queryAddCategories = "create table categories(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,name varchar(100) NOT NULL);";
 
-    public String queryAddCategories = "";
-
-    public String queryAddCategoriesSub = "";
+    public String queryAddCategoriesSub = "create table categories_sub(id int NOT NULL AUTO_INCREMENT PRIMARY KEY,main_id int NOT NULL,name varchar(100) NOT NULL, FOREIGN KEY(main_id) REFERENCES categories(id));";
 
 }
